35ef246d31a3f25095be794348122ad52e9959b5
Florian7843 <floriansch7843@gmail.com>
2022-06-09 23:16:15 +0200

    Fixed lists in dxs.__init__.py

--- pykostalpiko/dxs/__init__.py
+++ pykostalpiko/dxs/__init__.py
@@ -1,16 +1,16 @@
 """DxsEntries general constants and methods."""
 from typing import Any
-from pykostalpiko.dxs.current_values import LIST as current_values_LIST
+from pykostalpiko.dxs.current_values import LIST_ALL as current_values_LIST
 from pykostalpiko.dxs.entry import Descriptor
-from pykostalpiko.dxs.inverter import LIST as inverter_LIST
-from pykostalpiko.dxs.statistics import LIST as statistics_LIST
+from pykostalpiko.dxs.inverter import LIST_ALL as inverter_LIST
+from pykostalpiko.dxs.statistics import LIST_ALL as statistics_LIST
 
-LIST: list[Descriptor] = current_values_LIST + statistics_LIST + inverter_LIST
+LIST_ALL: list[Descriptor] = current_values_LIST + statistics_LIST + inverter_LIST
 
 
 def find_descriptor_by_id(dxs_id: int) -> Descriptor:
     """Find a descriptor by its id."""
-    for descriptor in LIST:
+    for descriptor in LIST_ALL:
         if descriptor.key == dxs_id:
             return descriptor
 

3bc8026ba44a90a9ff445e9a8098822e3ef8bcc1
Florian7843 <floriansch7843@gmail.com>
2022-06-09 22:58:35 +0200

    added method for simpler access to value in dict

--- pykostalpiko/dxs/__init__.py
+++ pykostalpiko/dxs/__init__.py
@@ -1,4 +1,5 @@
 """DxsEntries general constants and methods."""
+from typing import Any
 from pykostalpiko.dxs.current_values import LIST as current_values_LIST
 from pykostalpiko.dxs.entry import Descriptor
 from pykostalpiko.dxs.inverter import LIST as inverter_LIST
@@ -14,3 +15,8 @@ def find_descriptor_by_id(dxs_id: int) -> Descriptor:
             return descriptor
 
     raise ValueError(f"No descriptor found for id {dxs_id}")
+
+
+def get_value_by_descriptor(descriptor: Descriptor, data: dict) -> Any:
+    """Get the value of a descriptor from a data dictionary."""
+    return data[descriptor.name]
